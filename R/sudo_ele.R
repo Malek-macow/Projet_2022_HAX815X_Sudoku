5a93db5bfb54576722c8801df5dc7991a4960f9d
Eldohrim <alexcaapel@gmail.com>
2022-02-09 17:50:02 +0100

    Création de sudo_ele et mise en place.

---
+++ R/sudo_ele.R
@@ -0,0 +1,24 @@
+#' Produit un sudoku élémentaire de manière aléatoire
+#'
+#' @param None
+#' @return Une matrice 9x9 correspondant à un sudoku élémentaire
+#'
+
+
+
+sudo_ele <- function(){
+  A <- matrix(0, 9, 9)   # création de la matrice
+  A[1,] <- sample((1:9)) # on commence à générer une ligne aléatoire 
+  
+  # les trois blocs du haut
+  for (i in c(2,3)){
+    A[i,] <- c(A[i-1,4:9],A[i-1,1:3])
+  }
+  
+  seq <- c(2,3,1,5,6,4,8,9,7)
+  for (i in c(3,6)){
+    A[i + (1:3),] = A[(i-3) + (1:3),seq]
+  }
+  
+  return(A)  
+}
