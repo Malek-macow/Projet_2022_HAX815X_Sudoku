58d2725a497c66f16171fcf8c1b1f0fef429c2df
Malek Bouarroudj <67799348+Malek-macow@users.noreply.github.com>
2022-02-13 16:33:47 +0100

    Update permu_aleat.R

--- R/permu_aleat.R
+++ R/permu_aleat.R
@@ -1,6 +1,6 @@
 #' Permute aléatoirement une matrice 9x9 par bloc (3x9 et 9x3)
 #'
-#' @param Une matrice  9x9
+#' @param Matrice  9x9
 #' @return Une matrice 9x9 permuter
 #'
 
@@ -10,9 +10,9 @@
 
 permu_aleat = function (A){
 
-  for (i in c(0,3,6)){
+  for (i in c(0, 3, 6)){
   
-  A[, (1:3)+i] = A[, (1:3)+i][, sample(ncol(A[ ,(1:3)+i]))]
+  A[, (1:3)+i] = A[, (1:3)+i][, sample(ncol(A[, (1:3)+i]))]
   A[(1:3)+i, ] = A[(1:3)+i, ][sample(nrow(A[(1:3)+i, ])), ]
   }
   return(A)
