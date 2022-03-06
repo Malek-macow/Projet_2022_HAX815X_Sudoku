0a84ff79765907f1ca6246bfc79e4295e6a34831
Malek Bouarroudj <67799348+Malek-macow@users.noreply.github.com>
2022-03-06 22:12:40 +0100

    Update game.R
    désormais la difficulté dépend aussi du nombre de case vide

--- R/game.R
+++ R/game.R
@@ -10,35 +10,40 @@
 
 game <- function(diff){
   # détermination du nombre de backtracking en fonction de la difficulté
-  min <- 0
-  max <- 1               # valeurs par défaut
+  min <- 100
+  max <- 1000
+  N <- 45 # valeurs par défaut
 
   if (diff == "Facile"){
     min <- 0
     max <- 10
+    N <- 40
   }
 
   if (diff == "Moyen"){
     min <- 10
     max <- 100
+    N <- 45
   }
 
   if (diff == "Difficile"){
     min <- 100
     max <- 1000
+    N <- 50
   }
 
   if (diff == "Cauchemar"){
     min <- 1000
     max <- 10000
+    N <- 55
   }
 
-  A <- play_sudo(get_sudo())
+  A <- play_sudo(get_sudo(), N)
   B <- A
   res <- solve_sudo(B)
 
   while (res$nbr_ofback <min || res$nbr_ofback>max){
-    A <- play_sudo(get_sudo())
+    A <- play_sudo(get_sudo(), N)
     B <- A
     res <- solve_sudo(B)
   }
