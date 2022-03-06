d8f56a3b06c6f5093a318e4f757be7c81c11993f
Malek Bouarroudj <67799348+Malek-macow@users.noreply.github.com>
2022-03-06 22:26:34 +0100

    Update server.R

--- Sudoku4/server.R
+++ Sudoku4/server.R
@@ -8,6 +8,7 @@ shinyServer(function(input, output) {
                         sol = matrix(NA, 9,9),
                         mess = "Bonne partie ;)")
 
+
     observeEvent(input$Nouveau, {
         tout <- game(input$difficulte)
         v$affiche <- paste("Sudoku ", input$difficulte,
@@ -16,34 +17,61 @@ shinyServer(function(input, output) {
         v$sol <- tout$solution
         })
 
-    observeEvent(input$Solution, {
-        v$affiche <- "Voici la solution :"
+    observeEvent(input$Nouveau, {
+
+      shinyalert("Attention !", "Une fois vous commencez si vous regarder la solution la partie s'arrétera.",
+                 type ="info",showConfirmButton = T, confirmButtonText = "OK")
     })
+
+    observeEvent(input$Solution, {
+      v$affiche = "Voici la solution :"
+    })
+
 
     observeEvent(input$mAj, {
+      i = 9-floor(input$plot_click$y-0.5)
+      j = floor(input$plot_click$x-0.5)+1
         if(!all(is.na(v$sudo))){
-        i <- input$ligne
-        j <- input$colonne
-        val <- input$valeur
-        v$sudo[i, j] <- val
-        if (v$sudo[i,j]!=v$sol[i,j]){
-            v$sudo[i, j] <- NA
-            v$mess <- "Oops, ce n'est pas juste :'("
-        }else{
-            v$mess <- "Bien joué ! Continuez :)"
-    }
-    }
+          val <- input$valeur
+          v$sudo[i, j] <<- val
+          if (!is_valid(v$sudo, i, j, v$sudo[i,j])){
+            v$mess <- "Oops, ce n'est pas juste "
+            }else{
+              v$mess <- "Bien joué ! Continuez "
+            }
+          }
     })
+
+    observeEvent(input$mAJ, {
+      if (!is_valid(v$sudo, i, j, v$sudo[i,j])){
+        v$mess <- "Oops, ce n'est pas juste :'("
+      }else{
+        v$mess <- "Bien joué ! Continuez :)"
+      }
+      showNotification(v$mess)
+    })
+
+    observeEvent(input$sup, {
+      i = 9-floor(input$plot_click$y-0.5)
+      j = floor(input$plot_click$x-0.5)+1
+      v$sudo[i, j]  <- NA
+    })
+
     output$passudoku <- renderPrint({
-        return(v$affiche)
-        })
+      return(v$affiche)
+      })
 
-    output$sudoku <- renderPlot({
-        if (v$affiche == "Voici la solution :"){plot_sudoku(v$sol)}
-        else{plot_sudoku(v$sudo)}
-    }, width = 400, height = 400)
+    output$plot1 <- renderPlot({
+      if (v$affiche == "Voici la solution :"){plot_sudoku(v$sol)}
+      else{plot_sudoku(v$sudo)}
+    }, width = 500, height = 500)
 
-    output$juste <- renderPrint({
-        return(v$mess)
+    observeEvent(input$mAj, {
+      showNotification(v$mess)
+    })
+
+    output$info <- renderText({
+        paste0("numéro de la ligne :  ", 9-floor(input$plot_click$y-0.5),"\nnuméro de la colonne :  ", floor(input$plot_click$x-0.5)+1)
     })
 })
+
