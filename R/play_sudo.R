c5ffb88531db13df9c9005c0a093d0031e2de12d
Malek Bouarroudj <67799348+Malek-macow@users.noreply.github.com>
2022-03-06 22:14:07 +0100

    Update play_sudo.R

--- R/play_sudo.R
+++ R/play_sudo.R
@@ -1,17 +1,20 @@
 #' Algorithme qui masque certaines valeurs du sudoku pour commencer à jouer.
 #'
 #' @param A, Matrice 9x9
+#' @param N,  Nombre de valeurs manquantes dans le sudoku
 #' @return Une matrice 9x9 de sudoku prête à jouer.
 #'
 #'
 #' 
 
 
-play_sudo <- function(A){
-  for (i in (1:63)){
-    a <- sample((1:9),2,replace = T)
-    A[a[1],a[2]] = NA
+play_sudo <- function(A, N){
+  repeat {
+    A[sample(1:9,replace = TRUE, 1),sample(1:9,replace = TRUE, 1)] <- NA
+    if (length(which(is.na(A),arr.ind = F)) >= N){
+      break
+    }
   }
   return(A)
-  }
+}
 
