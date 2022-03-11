a1d4398e1061ad27d24aade4c1f582e7837a0c59
Eldohrim <alexcaapel@gmail.com>
2022-03-11 16:56:38 +0100

    Doc

--- Package/R/transfo.R
+++ Package/R/transfo.R
@@ -1,8 +1,19 @@
-#' Transforme une matrice 9x9 avec des NA en matrice 9x9 avec des zeros a la place.
+#' @title Transformation de matrice
+#'
+#' @description Cette fonction transforme une matrice 9x9 avec des NA en matrice
+#'  9x9 avec des zeros a la place.
 #'
 #' @param A Matrice  9x9
-#' @return Matrice 9x9 avec des zeros au lieu des NA
+#'
+#' @return Matrice 9x9 avec des zeros au lieu des NA.
+#'
 #' @export
+#'
+#' @examples
+#' A <- play_sudo(sudo_ele(), 45)
+#' print(transfo(A))
+
+
 transfo <- function(A) {
   for (i in 1:9) {
     for (j in 1:9) {
