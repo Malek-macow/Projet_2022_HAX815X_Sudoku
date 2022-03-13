16b3dbcf1a3564a0abce5bd352ba5a0c206b7a6c
YMalek-macow <bouarroudjmalek@gmail.com>
2022-03-13 16:33:22 +0100

    doc

--- Package/R/check.R
+++ Package/R/check.R
@@ -1,5 +1,18 @@
-
-
+#' @title Validation d'un sudoku
+#'
+#' @description Cette fonction verifie à la fin de la partie le sudoku est valide ou non
+#' (pas de nombre qui se repete dans une ligne, colonne ou bloc 3x3).
+#'
+#' @seealso Du package MALEX : is_valid
+#'
+#' @param A Sudoku (Matrice  9x9)
+#'
+#' @return TRUE si le sudoku est valide, FALSE sinon.
+#'
+#' @export
+#'
+#' @examples
+#' check(sudo_ele())
 check = function(A){
   H=matrix(0,9,9)
   if (isFALSE(all(!is.na(A)))){return(FALSE)}
